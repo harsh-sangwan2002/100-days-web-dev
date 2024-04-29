137312a22a77bdc97c56b8195bf8db082ea6a513
harsh-sangwan2003 <hsangwan2002@gmail.com>
2024-04-29 16:06:46 +0530

    added

---
+++ JavaScript/17. Map Filter Reduce/filter.js
@@ -0,0 +1,7 @@
+const arr = [1, 2, 3, 4, 5];
+
+// even values
+console.log(arr.filter(x => x % 2 === 0));
+
+// odd values
+console.log(arr.filter(x => x % 2 !== 0));
---
+++ JavaScript/17. Map Filter Reduce/map.js
@@ -0,0 +1,5 @@
+const arr = [1, 2, 3, 4, 5];
+
+console.log(arr.map(x => x * 2));
+console.log(arr.map(x => x * 3));
+console.log(arr.map(x => x.toString(2)));
