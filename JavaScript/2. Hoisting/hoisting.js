e0996754e4dd3145c80f1f57fa92c6a694504863
harsh-sangwan2003 <hsangwan2002@gmail.com>
2024-04-29 13:39:16 +0530

    added

--- JavaScript/2. Hoisting/hoisting.js
+++ JavaScript/2. Hoisting/hoisting.js
@@ -1,14 +1,16 @@
 console.log(getName);
 console.log(x);
+getName();
 
 // y will be hoisted as a variable and not as a function
 console.log(y);
 var x = 7;
 
+y();
 function getName() {
     console.log("Namaste Javascript.");
 }
 
-var y = function fn(){
+var y = function fn() {
     console.log("Anonymous function.");
 }
