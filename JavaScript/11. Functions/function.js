aee3b02ac3cd8557dc1e5f60d96f98244aa480bb
harsh-sangwan2003 <hsangwan2002@gmail.com>
2024-04-29 15:15:45 +0530

    added

---
+++ JavaScript/11. Functions/function.js
@@ -0,0 +1,19 @@
+// Function Statement
+function fn(){
+    console.log("Function statement.");
+}
+
+// Function Expression
+var b = function(){
+    console.log("Function expression.");
+}
+
+// Named Function Expression
+var c = function xyz(){
+    console.log("Named function expression.");
+}
+
+// First Class Function ->
+// They can be passed as an args 
+// They can be treated as variables
+// They can be returned
