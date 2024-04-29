0b0e0d5a18f14b8b4412c796707afdafa50b0d32
harsh-sangwan2003 <hsangwan2002@gmail.com>
2024-04-29 15:23:27 +0530

    added

---
+++ JavaScript/11. Functions/arrow.js
@@ -0,0 +1,11 @@
+// Arrow function -> Pure form of functions
+var x = function () {
+    console.log("Anonymous function.");
+}
+
+var y = () => {
+    console.log("Arrow function.")
+};
+
+var o1 = new x();
+// var o2 = new y();
--- JavaScript/11. Functions/function.js
+++ JavaScript/11. Functions/function.js
@@ -13,7 +13,7 @@ var c = function xyz(){
     console.log("Named function expression.");
 }
 
-// First Class Function ->
+// First Class Function/First Class Citizens  ->
 // They can be passed as an args 
 // They can be treated as variables
 // They can be returned
