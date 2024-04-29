6bce5ce8b4db8a2dcb200dd2489cd3d221fcf981
harsh-sangwan2003 <hsangwan2002@gmail.com>
2024-04-29 14:17:19 +0530

    added

---
+++ JavaScript/9. setTimeout/sol2.js
@@ -0,0 +1,15 @@
+function x() {
+
+    for (var i = 1; i <= 5; i++) {
+        function x(i) {
+            setTimeout(function () {
+                console.log(i);
+            }, i * 1000)
+        }
+        x(i);
+    }
+
+    console.log("Namaste Javascript");
+}
+
+x();
