bdcc6a4afb02b6cef344d9dfcd185af20e0a5809
harsh-sangwan2003 <hsangwan2002@gmail.com>
2024-05-05 21:14:03 +0530

    navbar & banner done

---
+++ React JS/Projects/movies-app/src/Components/Banner.js
@@ -0,0 +1,29 @@
+import React, { Component } from 'react'
+import { movies } from './Movies'
+
+export default class Banner extends Component {
+
+    render() {
+
+        let movie = movies.results[0];
+
+        return (
+            <>
+                {movie === '' ?
+
+                    <div class="spinner-border text-primary" role="status">
+                        <span class="visually-hidden">Loading...</span>
+                    </div> :
+
+                    <div className="card banner-card">
+
+                        <img src={`https://image.tmdb.org/t/p/original${movie.backdrop_path}`} className="card-img-top banner-img" alt={movie.title} />
+                        <h5 className="card-title banner-title">{movie.original_title}</h5>
+                        <p className="card-text banner-text">{movie.overview}</p>
+
+                    </div>
+                }
+            </>
+        )
+    }
+}
