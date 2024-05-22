c0848cb3e6354c1dac37de1ba927e75788756ae7
harsh-sangwan2003 <hsangwan2002@gmail.com>
2024-05-22 13:19:13 +0530

    created my own package

---
+++ NodeJS/Concepts/basics/third-party-modules/calc-math-opr/call.js
@@ -0,0 +1,4 @@
+const { add, sub } = require('./index');
+
+console.log(add(1, 2));
+console.log(sub(1, 2));
---
+++ NodeJS/Concepts/basics/third-party-modules/calc-math-opr/index.js
@@ -0,0 +1,12 @@
+module.exports = {
+
+    add: function (x = 0, y = 0) {
+
+        return x + y;
+    },
+
+    sub: function (x = 0, y = 0) {
+
+        return x - y;
+    }
+}
