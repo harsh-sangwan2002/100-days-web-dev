05cd8a4d94963df93b8afc69c38f862104f9b209
harsh-sangwan2003 <hsangwan2002@gmail.com>
2024-05-22 19:25:35 +0530

    updated api key

--- ReactJS/Projects/disney-clone/src/firebase.js
+++ ReactJS/Projects/disney-clone/src/firebase.js
@@ -9,7 +9,7 @@ import "firebase/compat/storage";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: process.env.FIREBASE_API__KEY,
+  apiKey: process.env.FIREBASE_API_KEY,
   authDomain: "disney-clone-b7200.firebaseapp.com",
   projectId: "disney-clone-b7200",
   storageBucket: "disney-clone-b7200.appspot.com",
