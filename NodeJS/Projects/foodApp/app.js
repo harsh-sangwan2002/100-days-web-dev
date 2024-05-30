87be12b67a41cb18078fd0da95e1e2675f8f7fde
harsh-sangwan2003 <hsangwan2002@gmail.com>
2024-05-30 10:55:26 +0530

    middlewares intro

--- NodeJS/Projects/foodApp/app.js
+++ NodeJS/Projects/foodApp/app.js
@@ -1,18 +1,37 @@
 const express = require('express');
 const app = express();
 
+// Everything in express is done through middlewares
+
+// This is a global middleware
 app.use(express.json());
 
 const authRouter = express.Router();
 
 app.use('/auth', authRouter);
 authRouter.route('/signup')
-    .get(getSignUp)
+    .get(middleware1, getSignUp, middleware2)
     .post(postSignUp);
 
-function getSignUp(req, res) {
+// req/res params are carried forward to the next middleware
+function middleware1(req, res, next) {
+
+    console.log("Middleware 1 called");
+    next();
+}
+
+function getSignUp(req, res, next) {
 
     res.sendFile('/public/index.html', { root: __dirname });
+    next();
+}
+
+function middleware2(req, res, next) {
+
+    console.log("Middleware 2 called & ended req/res cycle.");
+    res.json({
+        message: "Ended req/res cycle."
+    })
 }
 
 function postSignUp(req, res) {
