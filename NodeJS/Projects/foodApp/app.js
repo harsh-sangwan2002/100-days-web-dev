44cc222fa1de086ef4662b5625e215f4606772ce
harsh-sangwan2003 <hsangwan2002@gmail.com>
2024-05-30 12:03:50 +0530

    intro to mongoose & mongoDB

--- NodeJS/Projects/foodApp/app.js
+++ NodeJS/Projects/foodApp/app.js
@@ -1,6 +1,9 @@
 const express = require('express');
-const app = express();
+const mongoose = require('mongoose');
+const emailValidator = require('email-validator');
 
+const db_link = "mongodb+srv://harsh:Samay229@cluster0.oymn2pz.mongodb.net/food-app"
+const app = express();
 // Everything in express is done through middlewares
 
 // This is a global middleware
@@ -10,8 +13,10 @@ const authRouter = express.Router();
 
 app.use('/auth', authRouter);
 authRouter.route('/signup')
-    .get(middleware1, getSignUp, middleware2)
-    .post(postSignUp);
+    .get(getSignUp)
+    .post(postSignUp)
+    .patch(updateUser)
+    .delete(deleteUser);
 
 // req/res params are carried forward to the next middleware
 function middleware1(req, res, next) {
@@ -20,10 +25,17 @@ function middleware1(req, res, next) {
     next();
 }
 
-function getSignUp(req, res, next) {
+async function getSignUp(req, res, next) {
 
-    res.sendFile('/public/index.html', { root: __dirname });
-    next();
+    // get all the docs in userModel
+    let users = await userModel.find();
+
+    // get only one doc in userModel
+    let abhishek = await userModel.findOne({ name: "Abhishek" });
+
+    res.json({
+        abhishek
+    })
 }
 
 function middleware2(req, res, next) {
@@ -34,15 +46,77 @@ function middleware2(req, res, next) {
     })
 }
 
-function postSignUp(req, res) {
+async function postSignUp(req, res) {
 
-    let obj = req.body;
+    let user = req.body;
+    let data = await userModel.create(user);
 
     res.json({
-        message: "Data received successfully.",
-        data: obj
+        message: "User created successfully",
+        data
     })
 }
+
+async function updateUser(req, res) {
+
+    let dataToBeUpdated = req.body;
+    let user = await userModel.findOneAndUpdate({ email: "temp@gmail.com" }, dataToBeUpdated);
+
+    res.json({
+        message: "Data updated successfully"
+    })
+}
+
+async function deleteUser(req, res) {
+
+    let user = await userModel.findOneAndDelete({ email: "temp2@gmail.com" });
+
+    res.json({
+        message: "Data updated successfully",
+        user
+    })
+}
+
+mongoose.connect(db_link).then((db) => {
+    console.log("DB connected");
+}).catch(err => {
+    console.log(err);
+})
+
+const userSchema = mongoose.Schema({
+    name: {
+        type: String,
+        required: true
+    },
+    email: {
+        type: String,
+        required: true,
+        unique: true,
+        validate: function () {
+            return emailValidator.validate(this.email);
+        }
+    },
+    password: {
+        type: String,
+        required: true,
+        min: 8
+    },
+    confirmPassword: {
+        type: String,
+        required: true,
+        min: 8,
+        validate: function () {
+            return this.confirmPassword === this.password;
+        }
+    }
+})
+
+// Define the hooks brefore the userModel creation
+userSchema.pre('save', function () {
+    this.confirmPassword = undefined
+})
+
+const userModel = mongoose.model('userModel', userSchema);
 
 app.listen(3000, () => {
 
